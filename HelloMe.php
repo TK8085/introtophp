815035959b197d994d01bc1a248b7a0be1fe2922
KAWAMOTO-TAKESHITA <tk0987281@dt050581.otc.edu>
2024-08-28 09:12:48 -0500

    initial commit

---
+++ HelloMe.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$first_name = 'tate';
+$last_name = 'Kawamoto';
+
+$student = [
+    'degree' => 'CIS',
+    'transportation' => 'Bus',
+    'repo link' => 'git push -u origin main'
+];
+
+echo($first_name);
+print_r($first_name);
+echo($student);
+print_r($student);
